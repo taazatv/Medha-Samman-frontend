1b159ea7df32165891366b982c9d19e5ab932164
amitverma8835 <amitkv93047@gmail.com>
2025-07-02 17:37:37 +0530

    fixed error

--- src/components/MedhaData.tsx
+++ src/components/MedhaData.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "./navbar";
 import Footer from "./footer";
